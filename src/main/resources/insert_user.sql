39a1f8ad48f2acf5b29da670e87076a09b730616
toricor <irotoridoritoriyaben@yahoo.co.jp>
2018-02-02 22:51:32 +0900

    resources以下の移植

---
+++ src/main/resources/insert_user.sql
@@ -0,0 +1,4 @@
+INSERT INTO user VALUES(1, "name1", "password1", "2017-01-01 00:00:00");
+INSERT INTO user VALUES(2, "name2", "password2", "2017-01-01 00:00:01");
+INSERT INTO user VALUES(3, "name3", "password3", "2017-01-01 00:00:02");
+INSERT INTO user VALUES(4, "name4", "password4", "2017-01-01 00:00:03");
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,34 @@
+CREATE TABLE IF NOT EXISTS restaurant (
+    id              INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    station         VARCHAR(255) NOT NULL,
+    name            VARCHAR(255) NOT NULL UNIQUE,
+    genre           VARCHAR(255) NOT NULL,
+    lunch_or_dinner VARCHAR(25),
+    budget_lower    INTEGER,
+    budget_upper    INTEGER,
+    star            FLOAT NOT NULL,
+    good            VARCHAR(255) NOT NULL,
+    tabelog         VARCHAR(255),
+    created_at      datetime,
+    updated_at      datetime on update CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    INDEX(`name`),
+    INDEX(`star`)
+) ENGINE=InnoDB DEFAULT CHARACTER SET utf8;
+
+CREATE TABLE IF NOT EXISTS user (
+    id           INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name         VARCHAR(255) NOT NULL UNIQUE,
+    password     VARCHAR(255) NOT NULL,
+    created_at   datetime
+) ENGINE=InnoDB DEFAULT CHARACTER SET utf8;
+
+CREATE TABLE IF NOT EXISTS favorite (
+    id            INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    user_id       INTEGER NOT NULL,
+    restaurant_id INTEGER NOT NULL,
+    created_at    datetime,
+
+    INDEX `target_data` (`user_id`, `restaurant_id`),
+    foreign key(user_id) references user(id),
+    foreign key(restaurant_id) references restaurant(id)
+) ENGINE=InnoDB DEFAULT CHARACTER SET utf8;
